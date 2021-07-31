6ba39b69f508ccdbd2ced4e04d53a0ce32479ad6
parthsujalshah <parthsujalshah@gmail.com>
2021-07-31 13:48:01 +0530

    admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
 
-# Register your models here.
+from .models import (
+    Category,
+    Product,
+    ProductImage,
+    ProductSpecification,
+    ProductSpecificationValue,
+    ProductType,
+)
+
+admin.site.register(Category, MPTTModelAdmin)
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(ProductSpecification)
+admin.site.register(ProductSpecificationValue)
+admin.site.register(ProductType)
