874fa1bbf3231026249709cd2c20b7301391b159
parthsujalshah <parthsujalshah@gmail.com>
2021-08-02 09:54:15 +0530

    graphql url

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("store.urls", namespace="store"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("store.urls", namespace="store")),
+    path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
